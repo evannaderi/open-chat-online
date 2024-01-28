85bf06d85dc2e3a67cee1642d20debad0504af3a
Evan Naderi <evanvnaderi@gmail.com>
2024-01-28 15:08:47 -0500

    migrated

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  project_url TEXT := 'https://zyfamzoemzcaabggylal.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5ZmFtem9lbXpjYWFiZ2d5bGFsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNjQ2NzI4OSwiZXhwIjoyMDIyMDQzMjg5fQ.BdPs54_dcHZ3tan73aUoxo9GKSiT-341pcgJM3VJb3A'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
